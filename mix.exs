3eda3bb177a2cfba2172266823a4b5d962d40e8a
Sonny Scroggin <scrogson@gmail.com>
2017-01-12 21:45:08 -0600

    add exgravatar

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule NDC.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:comeonin, "~> 3.0"},
+     {:exgravatar, "~> 2.0"},
      {:cowboy, "~> 1.0"}]
   end
 
