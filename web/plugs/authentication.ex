04b560b78b5c6835bd4a95455328cb6eb622b776
Sonny Scroggin <scrogson@gmail.com>
2017-01-18 09:18:54 +0000

    don't pre-generate the user token

--- web/plugs/authentication.ex
+++ web/plugs/authentication.ex
@@ -5,11 +5,9 @@ defmodule NDC.Authentication do
 
   def call(conn, _opts) do
     user = user_from_session(conn)
-    token = user_token(conn, user)
 
     conn
     |> assign(:current_user, user)
-    |> assign(:current_user_token, token)
   end
 
   defp user_from_session(conn) do
@@ -18,9 +16,4 @@ defmodule NDC.Authentication do
       int -> NDC.User.get_user(int)
     end
   end
-
-  defp user_token(conn, %NDC.User{id: id}) do
-    Phoenix.Token.sign(conn, "user socket", id)
-  end
-  defp user_token(_, _), do: nil
 end
