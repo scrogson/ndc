7ba17a78a56d59334e0cc9f51205b98b0c8fe377
Sonny Scroggin <scrogson@gmail.com>
2017-01-18 10:23:04 +0000

    do generate the token stuff...no time

--- web/plugs/authentication.ex
+++ web/plugs/authentication.ex
@@ -5,9 +5,11 @@ defmodule NDC.Authentication do
 
   def call(conn, _opts) do
     user = user_from_session(conn)
+    token = user_token(conn, user)
 
     conn
     |> assign(:current_user, user)
+    |> assign(:current_user_token, token)
   end
 
   defp user_from_session(conn) do
@@ -16,4 +18,8 @@ defmodule NDC.Authentication do
       int -> NDC.User.get_user(int)
     end
   end
+
+  defp user_token(conn, %NDC.User{id: id}),
+    do: Phoenix.Token.sign(conn, "user socket", id)
+  defp user_token(conn, _), do: nil
 end
